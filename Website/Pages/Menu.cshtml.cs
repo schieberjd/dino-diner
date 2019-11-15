e6d78e749291160ab85a7fbfef426739edf2ef71
Justin Schieber <schieberjd@ksu.edu>
2019-11-15 16:29:04 -0600

    Completed Website Milestone 1

---
+++ Website/Pages/Menu.cshtml.cs
@@ -0,0 +1,25 @@
+﻿/* Menu.cs.html
+ * Author: Justin Schieber
+ */
+ 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using DinoDiner.Menu;
+
+namespace Website.Pages
+{
+    /// <summary>
+    /// Creates and controls the menu page of the website
+    /// </summary>
+    public class MenuModel : PageModel
+    {
+        /// <summary>
+        /// The current DinoDiner Menu
+        /// </summary>
+        public Menu Menu { get; } = new Menu();
+    }
+}
